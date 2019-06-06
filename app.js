d250e3c3f3ca8991c08157d2da803eb3b5ddf872
Marcos Oliveira <mavinii.cdc@gmail.com>
2019-06-06 17:08:56 +0100

    add pop up toastr when save some information

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 var express = require("express"); 
 var app = express();
-var port = 3000
+var bodyParser = require("body-parser");
+var port = 3000 //port server
 
 app.use(express.static("public"));      //para o express enchergar meu CSS da pgn public
-app.set("view engine", "ejs");          //nao precisa usar .ejs nas rotas, simplifica o codigo
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");          //no .ejs needed
+
+//provisorio porque usaremos banco de dados
+var rotas = [
+    {date: "14th apr", name: "Ann and Andrew", tel: "087-9694513", email:"annfriel61@gmail.com"},
+    {date: "20th apr", name: "Marcos and Aline", tel: "000-123456", email:"miltretamano@seila.com"}
+];
  
 //APP.JS MAIN PAGE NOT INDEX.ejs
 app.get('/', function(req, res){
@@ -40,22 +48,30 @@ app.get('/events', function(req, res){
 
 //ROTAS
 app.get('/rota', function(req, res){
-    var rotas = [
-        {date: "14th apr", name: "Ann and Andrew", tel: "087-9694513", email:"annfriel61@gmail.com"},
-        {date: "20th apr", name: "Marcos and Aline", tel: "000-123456", email:"miltretamano@seila.com"},
-        {date: "30th apr", name: "Ana and Eze", tel: "123-156489", email:"casalindo@gmail.com"},
-        {date: "50th apr", name: "Ethan and Sthep", tel: "321-321321", email:"irish@couple.com"}
-    ]
     res.render("rota", {rotas:rotas});
+});
+
+// IMPORTANT METHOD POST
+app.post('/rota', function(req, res){
+    // get data from form and add to /rota array
+    var date = req.body.date;
+    var name  = req.body.name;
+    var tel = req.body.tel;
+    var email = req.body.email;
+    var newRota = {date: date, name: name, tel: tel, email: email}
+    rotas.push(newRota);
+    //redirect back to compgrounds page
+    // res.redirect("/rota/painel");
+});
+
+// LOGIN ACEPT REDIRECT TO PAINEL
+app.get('/rota/painel', function(req, res){
+    res.render("painel");
 });
 
 app.get('/sermons', function(req, res){
     res.render("sermons");
 });
-
-// app.get('/blog', function(req, res){
-//     res.render("blog");
-// });
 
 app.get('/caterpillarKidz', function(req, res){
     res.render("caterpillarKidz");
@@ -68,11 +84,6 @@ app.get('/contacts', function(req, res){
 // login and password
 app.get('/login', function(req, res){
     res.render("login");
-});
-
-// LOGIN ACEPT REDIRECT TO PAINEL
-app.get('/painel', function(req, res){
-    res.render("painel");
 });
 
 //PAGE DOESN'T EXIST, this route need stay in the end!
