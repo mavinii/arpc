6a3d0dfa973f503151b0455ef43c678673e39147
Marcos Oliveira <pg.marcosoliveira@icloud.com>
2019-08-15 18:18:03 +0100

    Delete flowers.js

--- flowers.js
+++
@@ -1,9 +0,0 @@
-const mongoose  = require('mongoose');
-
-// FLOWERS Schema SETUP
-var flowersSchema = new mongoose.Schema({
-    date: String,
-    name: String
-});
-
-module.exports = mongoose.model("Flowers", flowersSchema);
